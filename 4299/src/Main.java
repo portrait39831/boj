edcb61c769dce6661119abac59e14bc2d0af7ac8
That_band <jangheejun2006@naver.com>
2025-09-01 11:06:17 +0900

    브4 4299 문제 해결 차(sub)가 합(sum)보다 크면 점수가 음수로 나옴 따라서 합이 차보다 무조건 크다는 조건을 넣어줘야 함

---
+++ 4299/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int sum = sc.nextInt();
+        int sub = sc.nextInt();
+
+        if ((sub + sum) / 2 + (sum - sub) / 2 == sum && (sub + sum) % 2 == 0 && sum >= sub) {
+            System.out.printf("%d %d", (sub + sum) / 2, (sum - sub) / 2);
+        }
+        else {
+            System.out.println("-1");
+        }
+    }
+}
