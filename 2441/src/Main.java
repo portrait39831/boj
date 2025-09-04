b827a66363a274981f926a40090290969165ef0e
That_band <jangheejun2006@naver.com>
2025-09-04 21:08:16 +0900

    브3 2441 문제 해결 별 찍기 고수가 될 거야

---
+++ 2441/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = n; j > i; j--) {
+                System.out.print("*");
+            }
+            if (i != n-1) {
+                System.out.println();
+            }
+
+        }
+    }
+}
