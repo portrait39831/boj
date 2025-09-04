89a9279e0c457823f931df19190bb0e305a5a425
That_band <jangheejun2006@naver.com>
2025-09-04 21:09:04 +0900

    브5 2438 문제 해결 별 찍기 고수가 될 거야

--- 2438/src/Main.java
+++ 2438/src/Main.java
@@ -10,7 +10,7 @@ public class Main{
             for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
-            System.out.println();
+            System.out.println( );
         }
     }
 }
