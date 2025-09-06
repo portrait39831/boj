125d908e3bd544c01bc8713ceb1806d63e7a6a86
That_band <jangheejun2006@naver.com>
2025-09-07 00:16:46 +0900

    브3 10991 문제 해결 별찍기 고수가 될거야

---
+++ 10991/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i < n + 1; i++) {
+
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
