ec0d6a78d1371152fbd67d8aa02e65bd6cc2c312
That_band <jangheejun2006@naver.com>
2025-09-10 15:48:38 +0900

    2522번 문제를 빠른 입출력과 String.repeat()로 다시 풀어봤음

---
+++ 2522/src/Main2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            bw.write(" ".repeat(n-i-1));
+            bw.write("*".repeat(i+1));
+            bw.newLine();
+        }
+        for (int i = 0; i < n - 1; i++) {
+            bw.write(" ".repeat(i+1));
+            bw.write("*".repeat(n-i-1));
+            bw.newLine();
+        }
+        bw.flush();
+    }
+}
