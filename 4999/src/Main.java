254fc807959648ddec2163010e026fc2178bb17d
That_band <jangheejun2006@naver.com>
2025-09-28 23:01:50 +0900

    브5 3046, 4101, 4999, 5337, 5338, 5339, 5341, 5522, 5717, 6840, 7287, 7891 문제 해결

---
+++ 4999/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String patient = sc.nextLine();
+        String doctor = sc.nextLine();
+
+        System.out.println(patient.length() >= doctor.length()
+                ? "go"
+                : "no" );
+    }
+}
---
+++ 7287/src/Main.java
@@ -0,0 +1,8 @@
+//자신의 아이디와 맞은 문제 개수를 출력하는 예능 문제
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(162); //풀 당시 맞은 문제 개수가 162개였음
+        System.out.println("portrait4521");
+    }
+}
