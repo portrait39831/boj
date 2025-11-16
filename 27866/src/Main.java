d4e4419e6e6dc8db2e2c2c2b39c90820c93074f7
That_band <jangheejun2006@naver.com>
2025-11-16 12:14:47 +0900

    브5 27866부터 28701까지 문제 해결

---
+++ 27866/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] s = sc.next().split("");
+        int i = sc.nextInt();
+
+        System.out.println(s[i-1]);
+    }
+}
