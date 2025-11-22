99bd39e589ba90d2668a3a1547fbd699d43bebc9
That_band <jangheejun2006@naver.com>
2025-11-22 16:14:52 +0900

    브5 29863, 30007, 30030, 30087, 30214, 30224, 30328, 30664, 30676 문제 해결

---
+++ 30214/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int s1 = sc.nextInt();
+        double s2 = sc.nextDouble();
+
+        if (s1 >= s2/2) {
+            System.out.println("E");
+        }
+        else {
+            System.out.println("H");
+        }
+    }
+}
