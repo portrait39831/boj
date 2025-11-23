56e4690404340c8308663aa822dd85d2663b92ed
That_band <jangheejun2006@naver.com>
2025-11-23 23:34:46 +0900

    브5 31429, 31450, 31606, 31610, 31611, 31614, 31654, 32154, 32314, 32326, 32384 문제 해결

---
+++ 0)언어 제한 문제(자바 X)/32260/src/Main.java
@@ -0,0 +1,12 @@
+/*
+언어 제한 때문에 C++로 풀어야 함
+제출에 A + B - 1로 되어있는데 그냥 - 1만 지우면 정답임;;
+
+#include "aplusb.h"
+
+int sum(int A, int B) {
+    return A + B - 1;
+}
+
+
+ */
---
+++ 31614/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+
+        System.out.println(h * 60 + m);
+    }
+}
